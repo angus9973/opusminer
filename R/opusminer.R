709c4cff5a052c4b95a09dc12516196ed129ad19
angus9973 <angus9973@users.noreply.github.com>
2017-01-17 13:28:22 +1100

    Include printed header (from original opus_miner.cpp)

--- R/opusminer.R
+++ R/opusminer.R
@@ -1,3 +1,15 @@
+# Header (from original opus_miner.cpp)
+opusHeader <- paste("OPUS Miner: Filtered Top-k Association Discovery of Self-Sufficient Itemsets",
+                    "Version 1.2",
+                    "Copyright (C) 2012-2016 Geoffrey I Webb",
+                    "This program comes with ABSOLUTELY NO WARRANTY. This is free software,",
+                    "and you are welcome to redistribute it under certain conditions.",
+                    "See the GNU General Public Licence <http://www.gnu.org/licenses/> for details.",
+                    "",
+                    "If you publish results obtained by using this software please cite:",
+                    "  Webb, G.I. & Vreeken, J. (2014) Efficient Discovery of the Most Interesting Associations.",
+                    "  ACM Transactions on Knowledge Discovery from Data. 8(3), Art. no. 15.")
+
 # Read itemlist-format files
 readItemList <- function(fileName) {
 
