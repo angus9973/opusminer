3dd7e75df574209da0e819aa93e0cff0243031c8
angus9973 <angus9973@users.noreply.github.com>
2017-01-17 14:47:21 +1100

    Modify print_itemsets.cpp to return output (to R) rather than printing to file

--- src/print_itemsets.cpp
+++ src/print_itemsets.cpp
@@ -5,15 +5,17 @@
 ** it under the terms of the GNU General Public License as published by
 ** the Free Software Foundation, either version 3 of the License, or
 ** (at your option) any later version.
-** 
+**
 ** This program is distributed in the hope that it will be useful,
 ** but WITHOUT ANY WARRANTY; without even the implied warranty of
 ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ** GNU General Public License for more details.
-** 
+**
 ** You should have received a copy of the GNU General Public License
 ** along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+
+#include <Rcpp.h>
 
 #include <stdio.h>
 #include <vector>
@@ -27,46 +29,50 @@ bool valgt(itemsetRec i1, itemsetRec i2) {
   return i1.value > i2.value;
 }
 
-void print_itemset(FILE *f, const itemset &is) {
+Rcpp::CharacterVector get_itemset(const itemset &is) {
+  Rcpp::CharacterVector items;
+
   itemset::const_iterator item_it;
 
   for (item_it = is.begin(); item_it != is.end(); item_it++) {
-    if (item_it != is.begin()) {
-      fputc(',', f);
-    }
-
-    fprintf(f, "%s", itemNames[*item_it].c_str());
+    items.push_back(itemNames[*item_it].c_str());
   }
+
+  return items;
 }
 
-void print_itemsetRec(FILE *f, const itemsetRec &is) {
-  print_itemset(f, is);
+Rcpp::GenericVector get_itemsetRec(const itemsetRec &is) {
+  Rcpp::CharacterVector items = get_itemset(is);
 
-  fprintf(f, " [%d,%f", is.count, is.value);
-  fprintf(f, " %g]", is.p);
+  // fprintf(f, " [%d,%f", is.count, is.value);
+  // fprintf(f, " %g]", is.p);
 
-  if (printClosures) {
-    itemset closure;
+  Rcpp::GenericVector record =
+    Rcpp::GenericVector::create(Rcpp::Named("Itemset") = items,
+                                Rcpp::Named("Count") = is.count,
+                                Rcpp::Named("Value") = is.value,
+                                Rcpp::Named("P") = is.p,
+                                Rcpp::Named("Closures") = "***",
+                                Rcpp::Named("Self-Sufficient") = "");
 
-    find_closure(is, closure);
+  // if (printClosures) {
+  //   itemset closure;
+  //
+  //   find_closure(is, closure);
+  //
+  //   if (closure.size() > is.size()) {
+  //     fprintf(f, " closure: ");
+  //     print_itemset(f, closure);
+  //   }
+  // }
 
-    if (closure.size() > is.size()) {
-      fprintf(f, " closure: ");
-      print_itemset(f, closure);
-    }
-  }
-
-  putc('\n', f);
+  // putc('\n', f);
+  return record;
 }
 
-void print_itemsets(FILE *f, std::vector<itemsetRec> &is) {
-  int i;
-  
-  for (i = 2; i < alpha.size(); i++) {
-      fprintf(f, "Alpha for size %d = %g\n", i, alpha[i]);
-  }
-
-  fprintf(f, "\nSELF-SUFFICIENT ITEMSETS:\n");
+Rcpp::GenericVector get_itemsets(std::vector<itemsetRec> &is) {
+  Rcpp::GenericVector output;
+  int index = 0;
 
   std::sort(is.begin(), is.end(), valgt);
 
@@ -79,16 +85,25 @@ void print_itemsets(FILE *f, std::vector<itemsetRec> &is) {
       failed_count++;
     }
     else {
-      print_itemsetRec(f, *it);
+      // print_itemsetRec(f, *it);
+      Rcpp::GenericVector record = get_itemsetRec(*it);
+      record("Self-Sufficient") = "YES";
+      output[index] = record;
+      index++;
     }
   }
 
   if (failed_count) {
-    fprintf(f, "\n%d itemsets failed test for self sufficiency\n", failed_count);
+    // fprintf(f, "\n%d itemsets failed test for self sufficiency\n", failed_count);
     for (it = is.begin(); it != is.end(); it++) {
       if (!it->self_sufficient) {
-        print_itemsetRec(f, *it);
+        // print_itemsetRec(f, *it);
+        Rcpp::GenericVector record = get_itemsetRec(*it);
+        record("Self-Sufficient") = "NO";
+        output[index] = record;
+        index++;
       }
     }
   }
+  return output;
 }
