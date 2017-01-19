1215f61151485d379f7acc8ba46e7b4e2e0fad44
angus9973 <angus9973@users.noreply.github.com>
2017-01-19 21:48:26 +1100

    Update file-reading functions

--- R/opusminer.R
+++ R/opusminer.R
@@ -128,8 +128,10 @@ opusR_piece <- function(fileName) {
       l <- readLines(f, n = 1)
       while(length(l) != 0) {
         v <- unlist(strsplit(l, "\\s+"))
-        n <- v[!(v %in% index)]
-        index <- c(index, n)
+        t1 <- system.time(n <- v[!(v %in% index)])
+        t2 <- system.time(index <- c(index, n))
+        print(t1)
+        print(t2)
         u <- match(v, index)
         # print(u)
         # print(ix)
@@ -149,69 +151,121 @@ opusR_piece <- function(fileName) {
 opusR_file_piece <- function(fileName) {
   index <- NULL
   ix <- as.integer(0)
+  # TT <- c(0, 0, 0)
+  TTB <- 0
   if (is.character(fileName) && file.exists(fileName)) {
     try({
       f <- file(fileName, open = "r")
       l <- readLines(f, n = 1)
       while(length(l) != 0) {
-        v <- unlist(strsplit(l, "\\s+"))
-        n <- v[!(v %in% index)]
-        index <- c(index, n)
-        u <- match(v, index)
+        raw <- strsplit(l, "\\s+")
+        # v <- unlist(raw, FALSE, FALSE)
+
+        # need to speed this up
+        # T1 <- Sys.time()
+        # n <- v[!(v %in% index)]
+        # T2 <- Sys.time()
+        # index <- c(index, n)
+        # T3 <- Sys.time()
+        # u <- match(v, index)
+        # T4 <- Sys.time()
+        #
+        # TT[1] <- TT[1] + (T2 - T1)
+        # TT[2] <- TT[2] + (T3 - T2)
+        # TT[3] <- TT[3] + (T4 - T3)
+
+
+        # index <- unique(c(index, v))
+        TB1 <- Sys.time()
+        # u <- match(v, index)
+        TB2 <- Sys.time()
+        #
+        TTB <- TTB + (TB2 - TB1)
+
+
         # print(u)
         # print(ix)
-        load_data_piece(u, ix)
+        # load_data_piece(u, ix)
         ix <- ix + 1
         l <- readLines(f, n = 1)
       }
       close(f)
     })
   }
+
+  print(TTB)
+
+  # tst
+  # return(index)
+
   # call cpp, return
   # decode return via index
   # return decoded
+  # print(TT)
 }
 
+# comparison, opus_miner.exe takes 4:42 to load kosarak.dat
+# this takes ~30s
+# or less?
 opusR_file_whole <- function(fileName) {
 
   tidList <- list()
+  TT <- list()
 
   if (is.character(fileName) && file.exists(fileName)) {
 
     try({
 
+      TT[[1]] <- Sys.time()
       f <- file(fileName, open = "r")
 
+      TT[[2]] <- Sys.time()
       # type = vector of character
       raw <- readLines(f)
 
+      TT[[3]] <- Sys.time()
       # print(typeof(raw))
       # print(class(raw))
       # print(mode(raw))
 
+
       # type = list of vector of character
       items <- strsplit(raw, "\\s+")
 
+      TT[[4]] <- Sys.time()
       # type = vector of character
       index <- unique(unlist(items, FALSE, FALSE))
 
+      TT[[5]] <- Sys.time()
       # replace item value (character) with index value (integer)
       # type = list of integer
       # items_int <- lapply(items, match, index)
-      # alternative (test if faster):
-      items_int <- unname(split(match(unlist(items), index),
-                          rep(seq(1, length(items), 1), lengths(items))))
+      # alternative (test if faster - looks like it):
+      items_int <- unname(
+                     split(
+                       match(unlist(items), index),
+                       rep(
+                         seq(1, length(items), 1),
+                         lengths(items)
+                         )
+                       )
+                     )
+      # above:
+      #
 
+      TT[[6]] <- Sys.time()
       # flatten
       # type = vector of integer
       items_int_flat <- unlist(items_int, recursive = FALSE, use.names = FALSE)
 
+      TT[[7]] <- Sys.time()
       # exchange item index value (integer) with transaction number (integer)
       # subtract 1 to index from 0 (for C++)
       # flatten
       # type = vector of integer
       trans <- rep(seq_along(items_int), lengths(items_int)) - as.integer(1)
 
+      TT[[8]] <- Sys.time()
       # forget the english meaning of "split"
       # split(a, b):
       # (1) returns a list
@@ -221,16 +275,114 @@ opusR_file_whole <- function(fileName) {
       # type = list of vector of integer
       tidList <- unname(split(trans, items_int_flat))
 
+      TT[[9]] <- Sys.time()
       close(f)
 
     })
   }
-  # load_data_whole(tidList)
+  load_data_whole(tidList)
+  TT[[10]] <- Sys.time()
   # toDO:
   # - call CPP
   # - reverse CPP value via index
 
+  # to check memory footprint in try({}) block, above
   # return(tidList)
+  # return(index)
+  # print(TT[[2]] - TT[[1]])
+  cat(format(diff(unlist(TT)), digits = 6), sep = "\n")
+}
+
+# comparison, opus_miner.exe takes 4:42 to load kosarak.dat
+# this takes ~30s
+# or less?
+opusR_file_whole_FAST <- function(fileName) {
+
+  tidList <- list()
+  TT <- list()
+
+  if (is.character(fileName) && file.exists(fileName)) {
+
+    try({
+
+      TT[[1]] <- Sys.time()
+      # f <- file(fileName, open = "r")
+      raw <- readChar(fileName, nchars = file.info(fileName)$size, useBytes = TRUE)
+
+      TT[[2]] <- Sys.time()
+      # type = vector of character
+      # raw <- readLines(f)
+
+      raw2 <- strsplit(raw, split = "\n", fixed = TRUE)
+
+      TT[[3]] <- Sys.time()
+      # print(typeof(raw))
+      # print(class(raw))
+      # print(mode(raw))
+
+
+      # type = list of vector of character
+      items <- strsplit(raw, "\\s+")
+
+      TT[[4]] <- Sys.time()
+      # type = vector of character
+      index <- unique(unlist(items, FALSE, FALSE))
+
+      TT[[5]] <- Sys.time()
+      # replace item value (character) with index value (integer)
+      # type = list of integer
+      # items_int <- lapply(items, match, index)
+      # alternative (test if faster - looks like it):
+      items_int <- unname(
+        split(
+          match(unlist(items), index),
+          rep(
+            seq(1, length(items), 1),
+            lengths(items)
+          )
+        )
+      )
+      # above:
+      #
+
+      TT[[6]] <- Sys.time()
+      # flatten
+      # type = vector of integer
+      items_int_flat <- unlist(items_int, recursive = FALSE, use.names = FALSE)
+
+      TT[[7]] <- Sys.time()
+      # exchange item index value (integer) with transaction number (integer)
+      # subtract 1 to index from 0 (for C++)
+      # flatten
+      # type = vector of integer
+      trans <- rep(seq_along(items_int), lengths(items_int)) - as.integer(1)
+
+      TT[[8]] <- Sys.time()
+      # forget the english meaning of "split"
+      # split(a, b):
+      # (1) returns a list
+      # (2) puts the elements in *a* in the position (list index) specified in *b*
+      # "swap" the transaction index with the item index number
+      # a bit like a "jagged" transpose of the transaction numbers
+      # type = list of vector of integer
+      tidList <- unname(split(trans, items_int_flat))
+
+      TT[[9]] <- Sys.time()
+      close(f)
+
+    })
+  }
+  load_data_whole(tidList)
+  TT[[10]] <- Sys.time()
+  # toDO:
+  # - call CPP
+  # - reverse CPP value via index
+
+  # to check memory footprint in try({}) block, above
+  # return(tidList)
+  # return(index)
+  # print(TT[[2]] - TT[[1]])
+  cat(format(diff(unlist(TT)), digits = 6), sep = "\n")
 }
 
 # getListIndex <- function(L) {
