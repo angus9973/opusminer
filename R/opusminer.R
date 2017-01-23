975dd6a3f0a6261b96fab23eec35711bd2f69011
angus9973 <angus9973@users.noreply.github.com>
2017-01-23 17:21:17 +1100

    Clean up R code

--- R/opusminer.R
+++ R/opusminer.R
@@ -10,6 +10,8 @@ opusHeader <- c("OPUS Miner: Filtered Top-k Association Discovery of Self-Suffic
                 "  Webb, G.I. & Vreeken, J. (2014) Efficient Discovery of the Most Interesting Associations.",
                 "  ACM Transactions on Knowledge Discovery from Data. 8(3), Art. no. 15.")
 
+# add: "convert" parameter, do conversion as in file-reading but on already-existing data
+# converstion itemlist -> tidlist
 opusR_ <- function(f = NULL,         # file
                    d = NULL,         # data
                    k = 100,          # k
@@ -25,9 +27,6 @@ opusR_ <- function(f = NULL,         # file
   k <- ifelse(k < 1, 1, k)
   p <- c(pc, fi, sl, cf, rd)
 
-  # check
-
-  # tmp <- NULL # to capture tidList for comparison
   output <- NULL
 
   if (is.character(f) && file.exists(f)) {
@@ -36,31 +35,27 @@ opusR_ <- function(f = NULL,         # file
 
       cat("Reading file...")
 
-      # T1 <- Sys.time()
       T1 <- proc.time()[3]
 
       # type = character
       raw <- readChar(f, file.info(f)$size, TRUE)
 
-      # strsplit returns list (of vector of character)
-      # in this case: list of length one; get first (and only) vector only
-      # type = vector of character
+      # type = vector of character (1st and only element of list of length 1)
       raw <- strsplit(raw, split = "\n", fixed = TRUE)[[1]]
 
+      # *** or strsplit(..., split = "\\s+") ***
       # type = list of vector of character
-      items <- strsplit(raw, " ", fixed = TRUE) # fixed vector of split = ...
+      items <- strsplit(raw, " ", fixed = TRUE)
 
-      # ***NEW*** (alt)
       noOfTransactions <- length(items)
 
       # type = vector of character
       index <- unique(unlist(items, FALSE, FALSE))
 
-      # ***NEW*** (alt)
       noOfItems <- length(index)
 
-      # replace item value (character) with index value (integer)
-      # much faster than "lapply(items, match, index)"
+      # replace item name (character) with item index (integer)
+      # faster than "lapply(items, match, index)"
       # type = list of integer
       items_int <- unname(
         split(
@@ -72,59 +67,40 @@ opusR_ <- function(f = NULL,         # file
         )
       )
 
-      # flatten
+      # intermediate steps to allow "jagged" transpose
+
       # type = vector of integer
       items_int_flat <- unlist(items_int, recursive = FALSE, use.names = FALSE)
 
-      # exchange item index value (integer) with transaction number (integer)
+      # exchange item index (integer) with transaction number (integer)
       # subtract 1 to index from 0 (for C++)
       # flatten
       # type = vector of integer
       trans <- rep(seq_along(items_int), lengths(items_int)) - as.integer(1)
 
       # C <- split(A, B): place each element of A, A[i], at C[[B[i]]]
-      # ie, "swap" the transaction number with the item index number
+      # ie, "swap" the transaction number with the item index
       # ie, like a "jagged" transpose of the transaction numbers and the item index nubmers
       # type = list of vector of integer
       tidList <- unname(split(trans, items_int_flat))
 
-      # T2 <- Sys.time()
       T2 <- proc.time()[3]
 
       cat(" (", round(T2 - T1, 2), " seconds)\n\n", sep = "")
       output <- .opusHelper(tidList, noOfItems, noOfTransactions, k, p)
 
-      # T3 <- Sys.time()
       T3 <- proc.time()[3]
 
       cat("Total time: ", round(T3 - T1, 2), " seconds.\n", sep = "")
 
-      # tmp <- tidList
-      # tmp <- index
-
-      # OPTIMISE... (duplicate/reverse split method, above?)
-      # Test for LARGE output sizes
-      # output <- lapply(output, function(i){i + 1}) # index from 1 (cf 0)
-      # output <- lapply(output, function(v){index[v]}) # "decode"
-
-      # repeat for each of itemset and closure,
-      # to avoid a separate function + copy
-
-      # or "output$itemset" / output[[]]
-      output$itemset <- lapply(output$itemset, function(i){i + 1}) # index from 1 (cf 0)
-      output$itemset <- lapply(output$itemset, function(v){index[v]}) # "decode"
+      output$itemset <- lapply(output$itemset, function(v){index[v + 1]}) # "decode"
 
       if (pc == FALSE) {
         output$closure <- NULL
       } else {
-        output$closure <- lapply(output$closure, function(i){i + 1}) # index from 1 (cf 0)
-        output$closure <- lapply(output$closure, function(v){index[v]}) # "decode"
+        output$closure <- lapply(output$closure, function(v){index[v + 1]}) # "decode"
       }
 
-      # or above two together:
-      # output <- lapply(output, function(v){index[v + 1]}) # "decode"
-
-      # T4 <- Sys.time()
       T4 <- proc.time()[3]
 
       cat("**Decode time: ", round(T4 - T3, 2), " seconds.**\n")
@@ -132,169 +108,17 @@ opusR_ <- function(f = NULL,         # file
     }) # try({...
   } # if (is.character(f) && file.exists(f)) {...
 
-  # output <- lapply(output, function(x){x + 1})
-  # reversed <- index[unlist(output)]
-
-  # return(list(output, tmp))
   return(output)
-}
-
-# Wrap opusCPP() (via opusHelper())
-opusR <- function(tidList,
-                  nItems,
-                  nTrans,
-                  printClosures = FALSE,
-                  filterItemsets = TRUE,
-                  k = 100,
-                  searchByLift = FALSE,
-                  correctForMultCompare = TRUE,
-                  redundancyTests = TRUE) {
-
-  # if (is.character(fileName) && file.exists(fileName)) {
-
-    # input <- readItemList(fileName)
-
-    k <- ifelse(k < 1, 1, k)
-
-    args <- c(printClosures,
-              filterItemsets,
-              searchByLift,
-              correctForMultCompare,
-              redundancyTests)
-
-    # cat(opusHeader, sep = "\n")
-
-    # output <- .opusHelper(input,
-    #                       k,
-    #                       args)
-
-    # load_data(input)
-
-    .opusHelper(tidList, nItems, nTrans, k, args)
-
-    # return(output)
-
-  # } else {
-  #   cat("ERROR")
-  # }
-}
-
-# comparison, opus_miner.exe takes 4:42 to load kosarak.dat
-# this takes ~30s
-# or less?
-opusR_file_whole_FAST <- function(fileName) {
-
-  # Doesn't need to be initialised (here or anywhere): restrict to "try" block
-  # tidList <- list()
-
-  if (is.character(fileName) && file.exists(fileName)) {
-
-    try({
-
-      # type = character
-      raw <- readChar(fileName,
-                      nchars = file.info(fileName)$size,
-                      useBytes = TRUE)
-
-      # strsplit returns list (of vector of character)
-      # in this case: list of length one; get first (and only) vector only
-      # type = vector of character
-      raw <- strsplit(raw, split = "\n", fixed = TRUE)[[1]]
-
-      # type = list of vector of character
-      items <- strsplit(raw, " ", fixed = TRUE) # fixed vector of split = ...
-      # items <- strsplit(raw, "\\s+") # regexp: slow
-
-      # ***NEW*** (alt)
-      numTrans <- length(items)
-
-      # # ***NEW***
-      # numItems <- length(items)
-
-      # type = vector of character
-      index <- unique(unlist(items, FALSE, FALSE))
-
-      # ***NEW*** (alt)
-      numItems <- length(index)
-
-      # replace item value (character) with index value (integer)
-      # much faster than "lapply(items, match, index)"
-      # type = list of integer
-      items_int <- unname(
-        split(
-          match(unlist(items), index),
-          rep(
-            # seq(1, length(items), 1), # slow
-            seq_along(items), # fast
-            lengths(items)
-          )
-        )
-      )
-
-      # flatten
-      # type = vector of integer
-      items_int_flat <- unlist(items_int, recursive = FALSE, use.names = FALSE)
-
-      # exchange item index value (integer) with transaction number (integer)
-      # subtract 1 to index from 0 (for C++)
-      # flatten
-      # type = vector of integer
-      trans <- rep(seq_along(items_int), lengths(items_int)) - as.integer(1)
-
-      # C <- split(A, B): place each element of A, A[i], at C[[B[i]]]
-      # ie, "swap" the transaction number with the item index number
-      # ie, like a "jagged" transpose of the transaction numbers and the item index nubmers
-      # type = list of vector of integer
-      tidList <- unname(split(trans, items_int_flat))
-
-      # # ***NEW***
-      # numTrans <- length(tidList)
-
-      # replace *tidList* with *unname(split(...))* ?
-      # load_data_whole(tidList, numItems, numTrans)
-
-      opusR(tidList, nItems = numItems, nTrans = numTrans)
-
-      # print(numItems)
-      # print(numTrans)
-
-    })
-  }
-  # Move up to try({...}) block...
-  # load_data_whole(tidList)
-
-  # return(opusR_2())
-  # opusR_2()
-  # toDO:
-  # - convert r object on cpp side properly
-  # - call CPP main function
-  # - reverse CPP value via index
-}
-
-# convert itemList to tidlist
-itemListToTIDList <- function (itemList) {
-  tidList <- list()
-  for (i in 1:length(itemList)) {
-    for (j in 1:length(itemList[[i]]))
-    {
-      # if k is an int --> works as an index of the list position
-      # if k is a chr --> works by naming the list entry lst["name"] = ...
-      k <- itemList[[i]][j]
-      if (k <= length(tidList)) {
-        # print(tidList[[k]])
-        tidList[[k]] <- c(tidList[[k]], i)
-      } else {
-        tidList[[k]] <- i
-      }
-      # tidList[[itemList[[i]][j]]] <- c(1, 3)
-    }
-  }
-  return(tidList)
 }
 
 getRecords <- function(itemset, index = NULL) {
   if (is.null(index)) {
     index = 1:length(itemset[[1]])
   }
-  return(as.data.frame(sapply(itemset, "[", index)))
+  tmp <- as.data.frame(sapply(itemset, "[", index))
+  tmp$count <- as.integer(tmp$count)
+  tmp$value <- as.numeric(tmp$value)
+  tmp$p <- as.numeric(tmp$p)
+  tmp$self_sufficient <- as.logical(tmp$self_sufficient)
+  return(tmp)
 }
